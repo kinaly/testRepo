71613642ab7039160ac18daf9c1f2df5b8631d0e
Kinaly at Aqovia <kinaly.venevongsos@aqovia.com>
2019-01-03 17:14:36 +0000

    [colour contrast explorer] Manually add some folders to the clean task

--- gulpfile-tasks/clean.js
+++ gulpfile-tasks/clean.js
@@ -2,7 +2,16 @@ var del = require('del');
 
 module.exports = function (paths) {
   return function () {
-  	var cleanList = paths.build.baseDir == "./" ? [paths.build.baseDir + '*.html', paths.build.styles, paths.build.js, paths.build.img] : paths.build.baseDir;
+  	var customCleanList = [
+  		paths.build.baseDir + '*.html',
+  		paths.build.baseDir + '/colour-contrast-explorer',
+  		paths.build.styles,
+  		paths.build.js,
+  		paths.build.baseDir + '/views',
+  		paths.build.img
+  	];
+
+  	var cleanList = paths.build.baseDir == "./" ? customCleanList : paths.build.baseDir;
   	var stream =
 	    del(
 	    	cleanList, 
