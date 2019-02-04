d500453a8213a657b9855e3801e8392c63700288
Kinaly VENEVONGSOS <hello@kinaly.net>
2019-02-04 23:50:34 +0000

    Remove files as they will now be pushed via buddy.works from the playground repo https://github.com/kinaly/playground

--- gulpfile-tasks/scripts.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (gulp, browserSync,  paths) {
-  return function () {
-  	var stream = 
-	    gulp.src(paths.src.js)
-  	  .pipe(gulp.dest(paths.build.js))
-    	.pipe(browserSync.stream());
-    	return stream;
-  };
-};
